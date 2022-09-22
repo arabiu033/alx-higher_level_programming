129d653b684e88edab0b7595641dda95586ecd08
arabiu033 <arabiu033@example.com>
2022-09-21 18:00:39 -0700

    Function to turn lower case to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end= '' if i != (len(str) - 1) else '\n')
-        else:
-            print("{:c}".format(ord(str[i])), end= '' if i != (len(str) - 1) else '\n')
+        print("{:c}".format(ord(str[i]) - 32 if ord(str[i]) >= 97 and ord(str[i]) <= 122 else ord(str[i])), end= '')
+    print()
