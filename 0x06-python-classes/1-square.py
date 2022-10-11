f07af0d5d2d931edeb12799fcd0d4d2a5d81515b
arabiu033 <arabiu033@gmail.com>
2022-10-11 15:03:07 -0700

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" No module Imported """
+""" No modules Imported """
 
 
 class Square:
+    """ A square class with a declared private variable """
     __size = 5
+
     def __init__(self, size):
         self.__size = size
