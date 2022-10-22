e44230e49c3199efff31a46d39ccfe41956fe56f
arabiu033 <arabiu033@gmail.com>
2022-10-21 18:59:36 -0700

    Bug fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,7 @@ def text_indentation(text=0):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    text = ".\n\n".join(text.split(". "))
-    text = "?\n\n".join(text.split("? "))
-    text = ":\n\n".join(text.split(": "))
+    text = ".\n\n".join(text.split("."))
+    text = "?\n\n".join(text.split("?"))
+    text = ":\n\n".join(text.split(":"))
     print(text, end="")
