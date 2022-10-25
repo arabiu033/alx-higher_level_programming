03ce86e5cc2db3aeb581f4077c81b0ae703a1eb3
arabiu033 <arabiu033@gmail.com>
2022-10-24 22:10:15 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/10-main.py~
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+Square = __import__('10-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
+
+guillaume@ubuntu:~/0x0A$ ./10-main.py
+[Rectangle] 13/13
+169
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Rectange module imported """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -6,12 +7,11 @@ class Square(Rectangle):
     """Type class of a square inherit a rectangle"""
 
     def __init__(self, size):
-
-        self.integer_validator("size", size)
+        """ Initializer """
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-
+        """ Work as string """
         string = "[Square] " + str(self.__size) + "/" + str(self.__size)
         return string
