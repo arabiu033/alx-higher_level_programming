a97fe08ead16ed126736965ade6d524522f9b2d0
arabiu033 <arabiu033@gmail.com>
2022-10-24 22:13:25 -0700

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+""" No module imported """
 
 class MyInt(int):
     """Type class of MyInt inherit int type"""
 
     def __eq__(self, other):
-
+        """ Opposite of parent method """
         return self.real != other
 
     def __ne__(self, other):
-
+        """ Opposit of parent method """
         return self.real == other
