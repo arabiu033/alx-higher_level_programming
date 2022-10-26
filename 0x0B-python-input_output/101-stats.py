2112335a392f59948a938c8e677893c3b483e7c4
Aminu Rabiu <arabiu033@gmail.com>
2022-10-26 05:59:41 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,6 +4,7 @@ reads stdin line by line and computes metrics
 """
 import sys
 
+
 file_size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
