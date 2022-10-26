352b8183c05c7b4925e904edac090f4f6593ce17
arabiu033 <arabiu033@gmail.com>
2022-10-25 20:20:02 -0700

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" No Module Imported """
+
+
+class Student:
+    """ Class to represent a student """
+
+    def __init__(self, fname, lname, age):
+        """ Intialize """
+        self.first_name = fname
+        self.last_name = lname
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if attrs is not None:
+            return dict((a, b) for (a, b) in self.__dict__.items() if a in attrs)
+        return self.__dict__
