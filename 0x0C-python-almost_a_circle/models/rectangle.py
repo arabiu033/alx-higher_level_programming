ed04db6cb73c57516afdf0d1e5f8bffc7a90a4ef
arabiu033 <arabiu033@gmail.com>
2022-10-30 01:47:01 -0700

    9. Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,20 +95,32 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                            self.width, self.height)
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ assigns an argument to each attribute """
-        if len(args) == 0:
-            return
-        self.id = args[0]
-        if len(args) == 1:
-            return
-        self.width = args[1]
-        if len(args) == 2:
-            return
-        self.height = args[2]
-        if len(args) == 3:
-            return
-        self.x = args[3]
-        if len(args) == 4:
-            return
-        self.y = args[4]
+        if len(args) != 0:
+            self.id = args[0]
+            if len(args) == 1:
+                return
+            self.width = args[1]
+            if len(args) == 2:
+                return
+            self.height = args[2]
+            if len(args) == 3:
+                return
+            self.x = args[3]
+            if len(args) == 4:
+                return
+            self.y = args[4]
+
+        elif len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
