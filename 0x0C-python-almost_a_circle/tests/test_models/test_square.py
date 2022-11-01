8b40c3e22f65ea8b2c25e264fdd1532845391961
arabiu033 <arabiu033@gmail.com>
2022-10-31 20:27:34 -0700

    All tests are passing

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ All required modules imported """
 import unittest
-from model.base import Base
-from model.square import Square
+from models.base import Base
+from models.square import Square
 
 
 class Test_instances(unittest.TestCase):
