d77dbff21b468480ac6148593c79195b9ee7cb21
arabiu033 <arabiu033@gmail.com>
2022-12-02 09:36:04 -0800

    0. List databases

--- 0x0D-SQL_introduction/3-list_tables.sql
+++
@@ -1,2 +0,0 @@
--- a script that lists all the tables of a database in your MySQL server.
-SHOW TABLES;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++
@@ -1,2 +0,0 @@
---  a script that inserts a new row in the table first_table
-INSERT INTO first_table VALUES (89, 'Best School');
