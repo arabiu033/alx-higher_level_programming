7c45ad8f1d67cb0be233aa8b0e3215b6ffe0d230
arabiu033 <arabiu033@gmail.com>
2022-12-02 16:41:02 -0800

    19. Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Import in hbtn_0c_0 database this table dump
 --  a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-Select name, value AS avg_temp
+Select city, value AS avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 ORDER BY avg_temp DESC
