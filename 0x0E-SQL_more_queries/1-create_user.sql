2d179db9f14347fd5b8b92964bc043b5eae58dfc
arabiu033 <arabiu033@gmail.com>
2022-12-03 06:35:15 -0800

    1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+--  a script that creates the MySQL server user user_0d_1.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILAGES
+ON *.*
+TO `user_0d_1`@`localhost`
