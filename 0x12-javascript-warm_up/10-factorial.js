341548d171016333e8ca228737589190ec542087
Aminu Rabiu <arabiu033@gmail.com>
2022-12-19 22:39:59 +0100

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-function fibo (val) {
+function fac (val) {
   if (val <= 1) { return 1; }
-  return fibo(val - 1) + fibo(val - 2);
+  return val * fac(val - 1);
 }
 
-console.log(fibo(isNaN(+argv[2]) ? 1 : +argv[2]));
+console.log(fac(isNaN(+argv[2]) ? 1 : +argv[2]));
