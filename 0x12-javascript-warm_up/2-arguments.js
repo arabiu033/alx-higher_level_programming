5862b170e01f4433cadd8ac875324e5a97e2e699
Aminu Rabiu <arabiu033@gmail.com>
2022-12-19 21:37:13 +0100

    2. Arguments2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const p = require('process');
-console.log(p.argv.length > 2 ? 'Argument found' : 'No argument');
+if (p.argv.length > 2) { console.log(p.argv.length === 3 ? 'Argument found' : 'Argument founds'); } else { console.log('No argument;='); }
