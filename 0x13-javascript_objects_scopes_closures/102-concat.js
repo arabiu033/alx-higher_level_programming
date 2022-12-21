9133750fb90b4719ba5887531129e5b5e7e3569b
Aminu Rabiu <arabiu033@gmail.com>
2022-12-21 03:41:22 +0100

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,3 @@ const fs = require('fs');
 
 fs.readFile(argv[2], (_, data) => { fs.appendFile(argv[4], data.toString(), (_) => {}); });
 fs.readFile(argv[3], (_, data) => { fs.appendFile(argv[4], data.toString(), (_) => {}); });
-
