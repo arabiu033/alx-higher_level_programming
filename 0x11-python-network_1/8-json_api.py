cb1c95560b7debbd9d348f9ef78d0022ff7fb648
arabiu033 <arabiu033@gmailcom>
2023-01-22 17:36:53 -0800

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
             print('No result')
         else:
             print('[{}] {}'.format(res.get('id'), res.get('name')))
-    except:
+    except ValueError:
         print("Not a valid JSON")
