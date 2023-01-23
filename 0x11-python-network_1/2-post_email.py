4fc2b1ab738253349a3f64d7a2d5366feb5ccb1e
arabiu033 <arabiu033@gmailcom>
2023-01-22 16:14:12 -0800

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""  a Python script that takes in a URL and an email, 
-sends a POST request to the passed URL with the email 
+"""  a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
 as a parameter, and displays the body of the response (decoded in utf-8) """
 
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     import sys
 
     url = sys.argv[1]
-    data = urllib.parse.urlencode({'email' : sys.argv[2]}).encode('ascii')
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
     full_url = urllib.request.Request(url, data)
     with urllib.request.urlopen(full_url) as res:
         print(res.read().decode('utf-8'))
