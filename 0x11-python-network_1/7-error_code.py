a02afe4ed5c26ba74a0d4f777e4f827016268782
arabiu033 <arabiu033@gmailcom>
2023-01-22 17:33:22 -0800

    8. Search API

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,16 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    d = requests.get(sys.argv[1])
-    if d.status_code >= 400:
-        print("Error code: {}".format(d.status_code))
-    else:
-        print(d.text)
+    val = ""
+    if len(sys.argv) > 1:
+        val = sys.argv[1]
+
+    d = requests.post('http://0.0.0.0:5000/search_user', data={'q': val})
+    try:
+        res = d.json()
+        if res == {}:
+            print('No result')
+        else:
+            print('[{}] {}'.format(res.get('id'), res.get('name')))
+    except:
+        print("Not a valid JSON")
