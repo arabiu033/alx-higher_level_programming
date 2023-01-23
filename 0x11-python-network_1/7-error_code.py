69a6360fffaf711ac3ff3faf1a6bf948feef60f5
arabiu033 <arabiu033@gmailcom>
2023-01-22 17:52:07 -0800

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    d = requests.get(sys.argv[1])
+    if d.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(d.text)
