7cf6959b505df505dfc4f309ae8279704346689d
Aminu Rabiu <arabiu033@gmail.com>
2023-02-11 03:44:31 +0100

    7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const req = require('request');
+const fetchUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+req(fetchUrl, (err, res, data) => {
+  if (err) console.log(err);
+  else {
+    JSON.parse(data).characters.forEach(ch => {
+      req(ch, (err, res, data) => console.log(err || JSON.parse(data).name));
+    });
+  }
+});
